ce9754bdab0d6d03c0bd064bac15ace786ed41f5
Giovanni <gbertao96@gmail.com>
2022-12-30 23:33:33 -0300

    JNI libc implementation + load

--- app/src/main/cpp/hello_lib.cpp
+++ app/src/main/cpp/hello_lib.cpp
@@ -1,4 +1,10 @@
 //
 // Created by Giovanni on 30/12/2022.
 //
+#include <jni.h>
+#include <string>
 
+extern "C" JNIEXPORT jint JNICALL
+Java_com_giovanni_example_hellofromc_MainActivity_helloFromC(JNIEnv *env, jobject thiz) {
+    return 515;
+}
